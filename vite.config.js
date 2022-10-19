35cbdc8aaf7a23e95f5187cfbb574a1e006e3c37
7suggest <107754404+7suggest@users.noreply.github.com>
2022-10-19 15:46:07 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,9 @@ module.exports = defineConfig({
         privacy: './privacy.html',
         disclamer: './disclamer.html',
         contact: './contact.html',
-        cleaner: './cleaner/index.html',
         babywalker: './baby-walker/index.html',
-        babywalker_buying_guide: './baby-walker/buying-guide.html',
-        babywalker_best_baby_walker_for_hardwood_floors: './baby-walker/best-baby-walker-for-hardwood-floors.html',
+        babywalkerbuyingguide: './baby-walker/buying-guide.html',
+        babywalkerbestbabywalkerforhardwoodfloors: './baby-walker/best-baby-walker-for-hardwood-floors.html',
       }
     }
   }
